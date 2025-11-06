b36bfe14eb2bfb8c90aaba217644dd90fdb9bc71
pokre <pokreman06@gmail.com>
2025-11-05 19:02:51 -0700

    fixed stupid linting errors

--- AgentFront/Agent.Api/src/App.tsx
+++ AgentFront/Agent.Api/src/App.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export default function App() {
   const auth = useAuth();
-  const email = auth.user?.profile.email
-  const ADMIN_EMAIL = "nathan.howell@students.snow.edu";
+  // const email = auth.user?.profile.email
+  // const ADMIN_EMAIL = "nathan.howell@students.snow.edu";
 
   if (auth.isLoading) {
     return <div>Loading...</div>;
