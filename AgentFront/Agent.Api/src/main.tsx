2961bd0d2a773e2ac90d18cf6e9858dc6155e68b
pokre <pokreman06@gmail.com>
2025-11-05 20:54:23 -0700

    removed the stuff for auth because I don't know why it doesn't work

--- AgentFront/Agent.Api/src/main.tsx
+++ AgentFront/Agent.Api/src/main.tsx
@@ -3,42 +3,42 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Toaster } from 'react-hot-toast';
 import App from './App.tsx'
-import { AuthProvider, useAuth } from "react-oidc-context";
-import React from 'react';
+// import { AuthProvider, useAuth } from "react-oidc-context";
+// import React from 'react';
 
-const oidcConfig = {
-  authority: "https://auth-dev.snowse.io/realms/DevRealm",
-  client_id: "nagent",
-  redirect_uri: "http://client.nagent.duckdns.org",
-  response_type: "code",
-};
+// const oidcConfig = {
+//   authority: "https://auth-dev.snowse.io/realms/DevRealm",
+//   client_id: "nagent",
+//   redirect_uri: "http://client.nagent.duckdns.org",
+//   response_type: "code",
+// };
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider {... oidcConfig}>
+    {/* <AuthProvider {... oidcConfig}> */}
     
       <Toaster position="top-right" reverseOrder={false} />
-      <RequireAuth>
         <App />
-      </RequireAuth>
-    </AuthProvider>
 
   </StrictMode>
 )
-function RequireAuth({ children }: { children: JSX.Element }) {
-  const auth = useAuth();
-  const [redirecting, setRedirecting] = React.useState(false);
+// function RequireAuth({ children }: { children: JSX.Element }) {
+  {/* <RequireAuth>
+  </RequireAuth> */}
+  {/* </AuthProvider> */}
+//   const auth = useAuth();
+//   const [redirecting, setRedirecting] = React.useState(false);
 
-  React.useEffect(() => {
-    if (!auth.isLoading && !auth.isAuthenticated && !redirecting) {
-      setRedirecting(true);
-      auth.signinRedirect().catch(err => console.error("Signin redirect error:", err));
-    }
-  }, [auth.isLoading, auth.isAuthenticated, auth, redirecting]);
+//   React.useEffect(() => {
+//     if (!auth.isLoading && !auth.isAuthenticated && !redirecting) {
+//       setRedirecting(true);
+//       auth.signinRedirect().catch(err => console.error("Signin redirect error:", err));
+//     }
+//   }, [auth.isLoading, auth.isAuthenticated, auth, redirecting]);
 
-  if (auth.isLoading) return <div>Loading...</div>;
-  if (!auth.isAuthenticated) return <div>Redirecting to login...</div>;
+//   if (auth.isLoading) return <div>Loading...</div>;
+//   if (!auth.isAuthenticated) return <div>Redirecting to login...</div>;
 
-  return children;
-}
+//   return children;
+// }
